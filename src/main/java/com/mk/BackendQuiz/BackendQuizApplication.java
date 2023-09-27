164c4f83e147db46c63332107886fbe730e34f87
Mouayad20 <moukad18@gmail.com>
2023-09-27 17:31:59 +0300

    environment setup Done.

---
+++ src/main/java/com/mk/BackendQuiz/BackendQuizApplication.java
@@ -0,0 +1,13 @@
+package com.mk.BackendQuiz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendQuizApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendQuizApplication.class, args);
+	}
+
+}
